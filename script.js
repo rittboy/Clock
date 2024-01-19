378ad7b8723b7126a40967f3e9631337926ba161
John Ritter <john.ritter.8788@gmail.com>
2024-01-19 05:23:55 -0500

    Updated documentation on JS file for clarity

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+/**
+ * takes current time and calculates location on clock face
+ * calculates location on clock face hands should display at based upon calculation included in function
+ */
 function setDate(){
-    const now = new Date();
 
+    const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
@@ -16,7 +20,7 @@ function setDate(){
     const hourDegrees = ((hour / 12) * 360) + ((mins/60) * 30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
-
+//Updates current time and displays every second
 setInterval(setDate, 1000);
 
 setDate();
